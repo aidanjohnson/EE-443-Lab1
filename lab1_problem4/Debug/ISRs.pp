7b811ebb6916fe0dc1dc7a78b99e6a660058f8f5
Aidan Johnson <34667756+aidanjohnson@users.noreply.github.com>
2018-04-03 15:21:04 -0700

    Add files via upload

--- lab1_problem4/Debug/ISRs.pp
+++ lab1_problem4/Debug/ISRs.pp
@@ -8,6 +8,15 @@ ISRs.obj: ../OMAPL138_defines.h
 ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/c6x.h
 ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/vect.h
 ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/stdint.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/math.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/float.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/access.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/linkage.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/elfnames.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathf.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathl.h
+ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h
 
 ../ISRs.c: 
 ../DSP_Config.h: 
@@ -17,3 +26,12 @@ ISRs.obj: C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/stdint.h
 C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/c6x.h: 
 C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/vect.h: 
 C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/math.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/float.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/access.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/elfnames.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathf.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/mathl.h: 
+C:/ti/ccsv6/tools/compiler/c6000_7.4.14/include/unaccess.h: 
